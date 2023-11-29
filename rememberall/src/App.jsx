4bd42855cbefcc55a9fe4bf97d33a1f24e444b47
Abe Linsenmayer <abe.linsenmayer@calabrio.com>
2023-11-29 15:47:43 -0600

    Added StrictModeDroppable to remediate known library issue. Added reordering handler

--- rememberall/src/App.jsx
+++ rememberall/src/App.jsx
@@ -1,23 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import ToDoList from './todolist/ToDoList'
 import { ToDo } from './todolist/ToDoListItem'
-import { DragDropContext } from 'react-beautiful-dnd'
+import { useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   const items = [
     new ToDo("Jumping jacks", false),
     new ToDo("Do the dishes", false)
   ]
 
   return (
-    <DragDropContext>
-      <ToDoList initItems={items} />
-    </DragDropContext>
+    <ToDoList initItems={items} />
   )
 }
 
--- rememberall/src/todolist/ToDoList.jsx
+++ rememberall/src/todolist/ToDoList.jsx
@@ -3,11 +3,9 @@ import CreateItemForm from './CreateItemForm'
 import { ToDoListItem, ToDo } from './ToDoListItem'
 import { Button } from '@mui/base'
 import { List } from '@mui/material'
-import { Droppable } from 'react-beautiful-dnd'
 import { v4 as uuid } from 'uuid'
-
-import { useEffect } from 'react'
-import React from 'react'
+import { DragDropContext } from 'react-beautiful-dnd'
+import { StrictModeDroppable } from '../StrictModeDroppable'
 
 export default function ToDoList({ initItems }) {
     const [items, setItems] = useState(initItems)
@@ -28,44 +26,44 @@ export default function ToDoList({ initItems }) {
     const clearCompleted = () => {
         setItems(items.filter((i) => i.done == false))
     }
-
-    // Fixes an issue with react-beautiful-dnd and StrictMode
-    // Source: https://medium.com/@wbern/getting-react-18s-strict-mode-to-work-with-react-beautiful-dnd-47bc909348e4
-    const [enabled, setEnabled] = useState(false);
-    useEffect(() => {
-        const animation = requestAnimationFrame(() => setEnabled(true));
-        return () => {
-            cancelAnimationFrame(animation);
-            setEnabled(false);
-        };
-    }, []);
-    if (!enabled) {
-        return null;
+    const moveItem = (fromIdx, toIdx) => {
+        const [itemToMove] = items.splice(fromIdx, 1)
+        items.splice(toIdx, 0, itemToMove)
+        setItems(items)
     }
-    // -----
+
+    // Drag handler
+    const handleDragEnd = (result) => {
+        if (!result.destination) {
+            return
+        }
+        moveItem(result.source.index, result.destination.index)
+    }
 
     return (
         <>
             <h1>To-Do List</h1>
-            <Droppable type='TO_DO_LIST' droppableId={`to-do-list-${uuid()}`}>
-                {(provided, snapshot) => (
-                    <div ref={provided.innerRef} {...provided.droppableProps} >
-                        <List>
-                            {items.map((item, index) =>
-                                <ToDoListItem
-                                    key={item.id}
-                                    todo={item}
-                                    toggleDone={toggleDone}
-                                    remove={remove}
-                                    updateItem={updateText}
-                                    index={index}
-                                />
-                            )}
-                        </List>
-                        {provided.placeholder}
-                    </div>
-                )}
-            </Droppable>
+            <DragDropContext onDragEnd={handleDragEnd}>
+                <StrictModeDroppable type='TO_DO_LIST' droppableId={`to-do-list-${uuid()}`}>
+                    {(provided, snapshot) => (
+                        <div ref={provided.innerRef} {...provided.droppableProps} >
+                            <List>
+                                {items.map((item, index) =>
+                                    <ToDoListItem
+                                        key={item.id}
+                                        todo={item}
+                                        toggleDone={toggleDone}
+                                        remove={remove}
+                                        updateItem={updateText}
+                                        index={index}
+                                    />
+                                )}
+                            </List>
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </StrictModeDroppable>
+            </DragDropContext>
             <br />
             <CreateItemForm addItem={addItem} />
             <Button variant="contained" onClick={clearCompleted}>Clear Complted</Button>
